32bfa8423d4b0b9bdacecc66e61734fc55cd4bac
Shu <sethanzai@gmail.com>
2017-10-10 21:52:52 -0400

    Bug fix with Powershell

--- upload-video.ps1
+++ upload-video.ps1
@@ -3,8 +3,9 @@ $dir = Split-Path $scriptpath
 Set-Location $dir
 
 
-[System.Windows.MessageBox]::Show('Please select video file.')
+Add-Type -AssemblyName PresentationFramework
 Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.MessageBox]::Show('Please select video file.')
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
     Multiselect = $false # Multiple files can be chosen
 	Filter = 'Video files (*.avi, *.m4v, *.mp4, *.mov)| *.avi; *.m4v; *.mp4; *mov' # Specified file types
